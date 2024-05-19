5bbd1c1b3339b85c5a7c75ba4f484ac15590de14
lyf2000 <lyf2000@mail.ru>
2024-05-19 17:13:34 +0300

    create  parallel threaded queues

--- consumer/main.py
+++ consumer/main.py
@@ -4,8 +4,13 @@ import os
 import aio_pika
 
 
+from common.utils import chunker
 from consumer.consumer import consume
 from consumer.schema import ConsumerMessage
+from common.settings import settings
+
+
+import multiprocessing
 
 
 async def process_message(
@@ -18,12 +23,24 @@ async def process_message(
         await consume(ConsumerMessage(**body))
 
 
-async def main() -> None:
-    connection = await aio_pika.connect_robust(
-        os.getenv("AMQP_URL"),
-    )
+def main() -> None:
+    queues_per_core = settings.QUEUE_N // multiprocessing.cpu_count()
+    if not queues_per_core:
+        queues_per_core = 1
 
-    queue_name = "message"
+    for queue_nums in chunker(range(settings.QUEUE_N), queues_per_core):
+        multiprocessing.Process(target=run_queues_per_core, args=(queue_nums,)).start()
+
+
+def run_queues_per_core(nums: list[int]):
+    for queue_num in nums:
+        asyncio.run(run_single_queue(queue_num))
+
+
+async def run_single_queue(num: int) -> None:
+    connection = await aio_pika.connect_robust(os.getenv("AMQP_URL"))
+
+    queue_name = f"send_{num}"
 
     # Creating channel
     channel = await connection.channel()
@@ -32,6 +49,7 @@ async def main() -> None:
     await channel.set_qos(prefetch_count=100)
 
     # Declaring queue
+    # queue = await channel.declare_exchange('message', aio_pika.ExchangeType.)
     queue = await channel.declare_queue(queue_name)
 
     await queue.consume(process_message)
@@ -44,4 +62,4 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
